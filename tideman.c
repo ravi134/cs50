992df67c47fd68e007b9a5a382498ac9229aac16
lodgedcandle <lodgedcandle@users.noreply.github.com>
2020-06-27 18:31:39 +0000

    automated commit by check50 [check50=True]

--- tideman.c
+++ tideman.c
@@ -194,21 +194,21 @@ void lock_pairs(void)
     {
         int row = pairs[i].winner;
         int col = pairs[i].loser;
-        bool cycle;
+        bool cycle = true;
 
         locked[row][col] = true;
 
-        do
+        for (int j = 0; j < pair_count; j++)
         {
-            for (int j = 0; j < pair_count; j++)
-                if (row == pairs[i - j].loser)
-                    if (row == pairs[0].winner)
-                    {
-                        locked[row][col] = false;
-                        cycle = false;
-                    }
+            if (row == pairs[i - j].loser)
+            {
+                if (row == pairs[0].winner)
+                {
+                    locked[row][col] = false;
+                    cycle = false;
+                }
+            }
         }
-        while(cycle == true);
     }
 }
 
