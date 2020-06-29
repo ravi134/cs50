bbcdfb46b2b9d66a27899ab0747b2f1aae13fd81
lodgedcandle <lodgedcandle@users.noreply.github.com>
2020-06-29 23:41:43 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -14,34 +14,31 @@ int main(void)
 {
     //declare integer n
     int n;
-
     // asks the user for a height between 1 and 8
     do
     {
         n = get_int("Pyramid Height: ");
     }
-    while (n <= 1 && n >= 8);
+    while (n < 1 || n > 8);
 
     // Main loop that prints the overall pyramid
     for (int height = 0; height < n; height++)
     {
         // Prints the spaces for the current row of the pyramid
-        for(int spaces = 0; spaces < n - height - 1; spaces++)
-        {
+        for (int spaces = 0; spaces < n - height - 1; spaces++)
             printf(" ");
-        }
+
         // Prints the #'s for the current row of the pyramid
         for (int shape = 0; shape <= height; shape++)
-        {
            printf("#");
-        }
+
         // Prints the two spaces in the middle of the pyramid
         printf("  ");
+
         // Prints the second set of #'s
         for (int shape = 0; shape <= height; shape++)
-        {
             printf("#");
-        }
+
         // Goes to the next line of the pyramid
         printf("\n");
     }
