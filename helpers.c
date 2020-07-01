067752293d32ec0f13abb0d966cc1519fddcd313
lodgedcandle <lodgedcandle@users.noreply.github.com>
2020-07-01 23:40:04 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -41,7 +41,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
             image[i][j].rgbtRed = image[i][width - j - 1].rgbtRed;
 
             image[i][width - j - 1].rgbtBlue = blue;
-            image[i][width - j - 1].rgbtBlue = green;
+            image[i][width - j - 1].rgbtGreen = green;
             image[i][width - j - 1].rgbtRed = red;
         }
     }
