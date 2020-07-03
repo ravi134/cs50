5f52deff2d4dbdf319d9f33f28ab045132dd7601
lodgedcandle <lodgedcandle@users.noreply.github.com>
2020-07-03 16:21:26 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -2,40 +2,49 @@
 #include <stdlib.h>
 #include <stdint.h>
 
+
 typedef uint8_t BYTE;
 FILE *pf;
 FILE *img;
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
-        printf("Usage: ./recover image\n");
+   if (argc != 2)
+  {
+    printf("Usage: ./recover image\n");
+  }
 
-    BYTE buffer[512];
-    pf = fopen(argv[1], "r");
-    int ct = 0;
-    char fname[8];
+ BYTE buffer[512];
+ pf = fopen(argv[1], "r");
+ int ct = 0;
+ char fname[8];
 
-    while(fread(buffer, 512, 1, pf))
-    {
-        if (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && buffer[3] && 0xf0 == 0xe0)
-        {
+  while(fread(buffer, 512, 1, pf) || (feof(pf) != 0))
+  {
 
-            if (img != NULL)
-            {
-                fclose(img);
-                ct += 1;
-            }
-
-            sprintf(fname,"%03i.jpg", ct);
-            img = fopen(fname,"w");
-            fwrite(buffer, sizeof(buffer), 1, img);
-        }
-        else if (img != NULL)
-            fwrite(buffer, sizeof(buffer), 1, img);
+      if  (buffer[0] == 0xff &&
+           buffer[1] == 0xd8 &&
+           buffer[2] == 0xff &&
+           (buffer[3] & 0xf0) == 0xe0)
+      {
+          if (img != NULL)
+          {
+              fclose(img);
+              ct += 1;
+          }
+          sprintf(fname,"%03i.jpg", ct);
+          img = fopen(fname,"w");
+          fwrite(buffer, sizeof(buffer), 1, img);
+      }
+      else
+      {
+          if (img != NULL)
+          {
+              fwrite(buffer, sizeof(buffer), 1, img);
+          }
     }
-
-    fclose(pf);
-    fclose(img);
-    return 0;
+  }
+  fclose(pf);
+  fclose(img);
+  return 0;
 }
