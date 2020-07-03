6f067959abfee5517c8999ba89d08dca37b49900
lodgedcandle <lodgedcandle@users.noreply.github.com>
2020-07-03 15:57:32 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -108,73 +108,63 @@ int edge(int a)
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
-    // sobel filters
     int Gx[3][3] = {{-1, 0, 1}, {-2, 0, 2}, {-1, 0, 1}};
     int Gy[3][3] = {{-1, -2, -1}, {0, 0 ,0}, {1, 2, 1}};
 
-    // creates copied image to story the original image and extra lines/columes
-    // of black pixels to loop through
-    RGBTRIPLE temp[height + 2][width + 2];
+    RGBTRIPLE t[height + 2][width + 2];
+
     for (int i = 0; i < height + 2; i++)
     {
         for (int j = 0; j < width + 2; j++)
         {
             if (i == 0 || j == 0 || i == height + 1 || j == width + 1)
             {
-                temp[i][j].rgbtRed = 0;
-                temp[i][j].rgbtGreen = 0;
-                temp[i][j].rgbtBlue = 0;
+                t[i][j].rgbtBlue = 0;
+                t[i][j].rgbtGreen = 0;
+                t[i][j].rgbtRed = 0;
             }
             else
-            {
-                temp[i][j] = image[i - 1][j - 1];
-            }
-        }
-    }
-    // second copied image with extra black pixels to store the result
-    RGBTRIPLE temp2[height + 2][width + 2];
-    for (int i = 0; i < height + 2; i++)
-    {
-        for (int j = 0; j < width + 2; j++)
-        {
-            temp2[i][j] = temp[i][j];
+                t[i][j] = image[i - 1][j - 1];
         }
     }
 
-    //calculation based on the copied image
+    RGBTRIPLE t2[height + 2][width + 2];
+
+    for (int i = 0; i < height + 2; i++)
+    {
+        for (int j = 0; j < width + 2; j++)
+            t2[i][j] = t[i][j];
+    }
+
     for (int i = 1; i < height + 1; i++)
     {
         for (int j = 1; j < width + 1; j++)
         {
-            float blueGx = 0.0, redGx = 0.0, greenGx = 0.0;
-            float blueGy = 0.0, redGy = 0.0, greenGy = 0.0;
+            float blueGx = 0.0, greenGx = 0.0, redGx = 0.0;
+            float blueGy = 0.0, greenGy = 0.0, redGy = 0.0;
 
             for (int k = -1; k < 2; k++)
             {
                 for (int h = -1; h < 2; h++)
                 {
+                    blueGx += t[i + k][j + h].rgbtBlue * Gx[k + 1][h + 1];
+                    greenGx += t[i + k][j + h].rgbtGreen * Gx[k + 1][h + 1];
+                    redGx += t[i  + k][j + h].rgbtRed * Gx[k + 1][h + 1];
 
-                    blueGx += temp[i + k][j + h].rgbtBlue * Gx[k + 1][h + 1];
-                    redGx += temp[i  + k][j + h].rgbtRed * Gx[k + 1][h + 1];
-                    greenGx += temp[i + k][j + h].rgbtGreen * Gx[k + 1][h + 1];
-
-                    blueGy += temp[i + k][j + h].rgbtBlue * Gy[k + 1][h + 1];
-                    redGy += temp[i + k][j + h].rgbtRed * Gy[k + 1][h + 1];
-                    greenGy += temp[i + k][j + h].rgbtGreen * Gy[k + 1][h + 1];
+                    blueGy += t[i + k][j + h].rgbtBlue * Gy[k + 1][h + 1];
+                    greenGy += t[i + k][j + h].rgbtGreen * Gy[k + 1][h + 1];
+                    redGy += t[i + k][j + h].rgbtRed * Gy[k + 1][h + 1];
                 }
             }
-
-            temp2[i][j].rgbtRed = edge(round(sqrt(redGx * redGx + redGy * redGy)));
-            temp2[i][j].rgbtBlue = edge(round(sqrt(blueGx * blueGx + blueGy * blueGy)));
-            temp2[i][j].rgbtGreen = edge(round(sqrt(greenGx * greenGx + greenGy * greenGy)));
+            t2[i][j].rgbtBlue = edge(round(sqrt(blueGx * blueGx + blueGy * blueGy)));
+            t2[i][j].rgbtGreen = edge(round(sqrt(greenGx * greenGx + greenGy * greenGy)));
+            t2[i][j].rgbtRed = edge(round(sqrt(redGx * redGx + redGy * redGy)));
         }
     }
 
     for (int i = 0; i < height; i++)
     {
         for (int j = 0; j < width; j++)
-        {
-            image[i][j] = temp2[i + 1][j + 1];
-        }
+            image[i][j] = t2[i + 1][j + 1];
     }
 }
