cbf6893ea94689cb0f8ff541377c45f8de8c5e72
lodgedcandle <lodgedcandle@users.noreply.github.com>
2020-07-03 16:37:03 +0000

    automated commit by check50 [check50=True]

--- recover.c
+++ recover.c
@@ -1,85 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define BLOCK 512
 
 int main(int argc, char *argv[])
 {
-
-    //check to make sure image is in command line
     if (argc != 2)
     {
         printf("Usage: ./recover file\n");
         return 1;
     }
 
-    //open image and check to make sure image is valid
     FILE *cardraw = fopen(argv[1], "r");
-
-    //exit with error if invalid
     if (cardraw == NULL)
     {
         fprintf(stderr, "Could not open %s.\n", argv[1]);
         return 1;
     }
 
-    //establish the buffer
-    unsigned char buffer[BLOCK];
-
-    //image counter
-    int counter = 0;
-
-    //file pointer to write the JPEGS too
+    unsigned char buf[BLOCK];
+    int cnt = 0;
     FILE *jpegs;
+    char in[8];
 
-    //array to hold file name
-    char imagename[8];
-
-    //repeat until end of card
-    while (fread(buffer, 1, BLOCK, cardraw) == BLOCK)
+    while (fread(buf, 1, BLOCK, cardraw) == BLOCK)
     {
-        //read 512 Bytes into a buffer and check if its the start of a jpeg
-        if (buffer[0] == 0xff
-            && buffer[1] == 0xd8
-            && buffer[2] == 0xff
-            && (buffer[3] & 0xf0) == 0xe0)
+        if (buf[0] == 0xff &&
+            buf[1] == 0xd8 &&
+            buf[2] == 0xff &&
+            (buf[3] & 0xf0) == 0xe0)
         {
-            //if first jpeg
-            if (counter == 0)
+            if (cnt == 0)
             {
-                //make the first file
-                sprintf(imagename, "%03i.jpg", counter);
-                counter ++;
-
-                //open newfile for writing
-                jpegs = fopen(imagename, "w");
-
-                // Write to outfile
-                fwrite(buffer, 1, BLOCK, jpegs);
+                sprintf(in, "%03i.jpg", cnt);
+                cnt ++;
+                jpegs = fopen(in, "w");
+                fwrite(buf, 1, BLOCK, jpegs);
             }
-            else if (counter > 0)
+            else if (cnt > 0)
             {
-                //close the last file we were writing to
                 fclose(jpegs);
-
-                //make the next file
-                sprintf(imagename, "%03i.jpg", counter);
-                counter ++;
-
-                //open newfile for writing
-                jpegs = fopen(imagename, "w");
-
-                // Write to outfile
-                fwrite(buffer, 1, BLOCK, jpegs);
-
+                sprintf(in, "%03i.jpg", cnt);
+                cnt ++;
+                jpegs = fopen(in, "w");
+                fwrite(buf, 1, BLOCK, jpegs);
             }
-
         }
-
-        else if (counter > 0)
-        {
-            fwrite(buffer, 1, BLOCK, jpegs);
-        }
+        else if (cnt > 0)
+            fwrite(buf, 1, BLOCK, jpegs);
     }
     fclose(jpegs);
     fclose(cardraw);
