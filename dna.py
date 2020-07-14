56ce72ff0539d0e489b8f94d2002526c40b9c501
lodgedcandle <lodgedcandle@users.noreply.github.com>
2020-07-14 05:27:18 +0000

    automated commit by check50 [check50=True]

--- dna.py
+++ dna.py
@@ -2,67 +2,58 @@ from csv import reader, DictReader
 from sys import argv
 
 if len(argv) < 3:
-    print("usage error, dna.py sequence.txt database.csv")
+    print("Usage Error: specify sequence and database please")
     exit()
 
-# read the dna sequence from the file
-with open(argv[2]) as dnafile:
-    dnareader = reader(dnafile)
-    for row in dnareader:
+# Get sequence from file
+with open(argv[2]) as dnaFile:
+    dnaReader = reader(dnaFile)
+    for row in dnaReader:
         dnalist = row
 
-# store it in a string
 dna = dnalist[0]
-# create a dictionary where we will store the sequences we intend to count
 sequences = {}
 
-# extract the sequences from the database into a list
-with open(argv[1]) as peoplefile:
-    people = reader(peoplefile)
-    for row in people:
+# Put sequence into list
+with open(argv[1]) as pplFile:
+    ppl = reader(pplFile)
+    for row in ppl:
         dnaSequences = row
         dnaSequences.pop(0)
         break
 
-# copy the list in a dictionary where the genes are the keys
-for item in dnaSequences:
-    sequences[item] = 1
+# Copy list to dict
+for i in dnaSequences:
+    sequences[i] = 1
 
-# iterate trough the dna sequence, when it finds repetitions of the values from sequence dictionary it counts them
+# Count STR's
 for key in sequences:
     l = len(key)
-    tempMax = 0
-    temp = 0
+    tMax = 0
+    tmp = 0
     for i in range(len(dna)):
-        # after having counted a sequence it skips at the end of it to avoid counting again
-        while temp > 0:
-            temp -= 1
+        while tmp > 0:
+            tmp -= 1
             continue
-
-        # if the segment of dna corresponds to the key and there is a repetition of it we start counting
         if dna[i: i + l] == key:
             while dna[i - l: i] == dna[i: i + l]:
-                temp += 1
+                tmp += 1
                 i += l
+            if tmp > tMax:
+                tMax = tmp
 
-            # it compares the value to the previous longest sequence and if it is longer it overrides it
-            if temp > tempMax:
-                tempMax = temp
+    # longest seq
+    sequences[key] += tMax
 
-    # store the longest sequences in the dictionary using the correspondent key
-    sequences[key] += tempMax
-
-# open and iterate trough the database of people treating each one like a dictionary so it can compare to the sequences one
+# Check for match
 with open(argv[1], newline='') as peoplefile:
     people = DictReader(peoplefile)
     for person in people:
         match = 0
-        # compares the sequences to every person and prints name before leaving the program if there is a match
         for dna in sequences:
             if sequences[dna] == int(person[dna]):
                 match += 1
         if match == len(sequences):
             print(person['name'])
             exit()
-
     print("No match")
