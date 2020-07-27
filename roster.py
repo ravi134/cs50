b8fd9a3296a7f6cd97dead9e5a784a5aedfa8f00
lodgedcandle <lodgedcandle@users.noreply.github.com>
2020-07-27 06:53:42 +0000

    automated commit by check50 [check50=True]

--- roster.py
+++ roster.py
@@ -1,18 +1,43 @@
-from cs50 import SQL
-from sys import argv, exit
+import sqlite3
+import csv
+import sys
 
-db = SQL("sqlite:///students.db")
-house = argv[1]
+def main():
 
-if len(argv) != 2:
-    print("Please specify the file")
-    exit(1)
+    # checking argv (needs to be one, house name)
+    if (len(sys.argv) != 2):
+        sys.exit("Usage: roster.py house_name")
 
-rows = db.execute("SELECT * FROM students WHERE house = ? ORDER BY last, first", house)
+    # Make house name lower to avoid cap sensitivity
+    housename = sys.argv[1].lower()
 
-for row in rows:
-    first = row["first"]
-    middle = row["middle"]
-    last = row["last"]
-    birth = row["birth"]
-    print(f"{first} {middle + ' ' if middle else ''} {last} born {birth}")
+    # Check if argument is indeed a house at Hogwarts
+    houses = ["slytherin", "gryffindor", "ravenclaw", "hufflepuff"]
+
+    if housename.lower() not in houses:
+        sys.exit("provide house name: Gryffindor, Hufflepuff, Slytherin or Ravenclaw.")
+
+    # Connect with the .db file and make a cursor
+    sqlite_file = "students.db"
+    con = sqlite3.connect(sqlite_file)
+
+    cur = con.cursor()
+
+    cur.execute('SELECT first, middle, last, birth FROM students WHERE lower(house) = "{}" ORDER BY last, first;'.format(housename))
+
+    # Fetchall gives us all the rows of the table as a list of tuples with strings.
+    houseroster = cur.fetchall()
+
+    # Do stuff with each row in table
+    for row in houseroster:
+
+        if not row[1]:
+            print("{} {}, born {}".format(row[0], row[2], row[3]))
+        else:
+            print("{} {} {}, born {}".format(row[0], row[1], row[2], row[3]))
+
+    con.close()
+
+
+if __name__ == "__main__":
+    main()
